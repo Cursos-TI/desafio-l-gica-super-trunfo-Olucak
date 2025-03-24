ba7f0506aea37dad95601849787d4fd14fea7b2f
Olucak <olucas419@gmail.com>
2025-03-24 12:05:13 +0000

    correções e analise final

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,11 +6,55 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
+    // Definição das variáveis para armazenar as propriedades das cidades,
+    char Estado[50],Estado2[50];
+    char codigo[4],codigo2[4];
+    char cidade[50],cidade2[50];
+    int populacao,populacao2;
+    float area,area2;
+    float pib,pib2;
+    int pontosturistico,pontosturistico2;
+    float densidade_populacional,densidade_populacional2;
+    float per_capita,per_capita2;
     // Você pode utilizar o código do primeiro desafio
 
     
     // Cadastro das Cartas:
+    printf ("Bem vindo ao Jogo Super Trunfo. \n"); 
+    printf("\n");
+    printf("Coloque os dados da primeira carta!\n");
+    printf("Insira o Estado: ");
+    scanf("%s",Estado);
+    printf("Insira o codigo da carta: ");
+    scanf("%s", codigo);
+    printf("Insira o nome da cidade: ");
+    scanf("%s", cidade);
+    printf("Insira a população: ");
+    scanf("%ld", &populacao);
+    printf("Insira a área em km²: ");
+    scanf("%f", &area);
+    printf("Insira o PIB: ");
+    scanf("%f", &pib);
+    printf("Quantidade de pontos turísticos: ");
+    scanf("%d", &pontosturistico);
+    printf("\n");
+
+    printf ("Agora coloque os dados da segunda carta! \n");
+    printf("Insira o Estado: ");
+    scanf("%s",Estado2);
+    printf("Insira o codigo da carta: ");
+    scanf("%s", codigo2);
+    printf("Insira o nome da cidade: ");
+    scanf("%s", cidade2);
+    printf("Insira a população: ");
+    scanf("%ld", &populacao2);
+    printf("Insira a área em km²: ");
+    scanf("%f", &area2);
+    printf("Insira o PIB: ");
+    scanf("%f", &pib2);
+    printf("Quantidade de pontos turísticos: ");
+    scanf("%d", &pontosturistico2);
+    printf("\n");
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
     // utilizando a função scanf para capturar as entradas.
     // utilize o código do primeiro desafio
